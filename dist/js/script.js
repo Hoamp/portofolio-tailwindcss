cd3839ea80e1b47bb12939743ba13416258541c6
Hoamp <setiawanthomas43@gmail.com>
2022-11-14 16:57:13 +0700

    menambah fitur to-top

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,15 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedNav){
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
+        toTop.classList.add('hidden');
+        toTop.classList.remove('flex');
         header.classList.remove('navbar-fixed');
     }
 }
@@ -17,4 +22,12 @@ const navMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+});
+
+// klik di luar hamburger
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target !== navMenu){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
 });
